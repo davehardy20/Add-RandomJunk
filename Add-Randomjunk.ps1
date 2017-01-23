9f1327c93712fb4f83aee5d29b2dd5ad630eb19c
davehardy20 <davehardy20@gmail.com>
2017-01-23 20:34:55 +0000

    Parameter changes

--- Add-Randomjunk.ps1
+++ Add-Randomjunk.ps1
@@ -1,4 +1,5 @@
-﻿Function Add-RandomJunk{
+﻿Function Add-RandomJunk
+{
     <#
             .Synopsis
             Add random junk to a file and append a random suffix to the file name.
@@ -28,34 +29,39 @@
 
 
     #>
-param (
-    [int]$Length, #length of random junk added to begining of file
-    [String]$path, #source of files
-    [String]$newpath, #place of junkified files
-    [String]$suffix=10 #length of the file name suffix
-)
+    param (
+        [Parameter(Mandatory=$true)][int]$Length, #length of random junk added to begining of file
+        [Parameter(Mandatory=$true)][String]$path, #source of files
+        [Parameter(Mandatory=$true)][String]$newpath, #place of junkified files
+        [int]$suffix = 10 #length of the file name suffix
+    )
 
-$set    = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!£$%^&*()_+}{[]#~'@;:".ToCharArray()
-$result = ""
-$Jitter = Get-Random -Minimum -50 -Maximum 50 #randomise the length of the added junk
-$RandomLength = $Length - $Jitter
-for ($x = 0; $x -lt $RandomLength; $x++) {
-    $result += $set | Get-Random 
-}
-$begin = "<#`n"
-$end = "`n#>`n"
-$final = $begin+$result+$end
-$randomfilename = -join ((65..90) + (97..122) | get-random -Count $suffix | % {[char]$_})
+    $set    = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!£$%^&*()_+}{[]#~'@;:".ToCharArray()
+    $result = ''
+    $Jitter = Get-Random -Minimum -50 -Maximum 50 #randomise the length of the added junk
+    $RandomLength = $Length - $Jitter
+    for ($x = 0; $x -lt $RandomLength; $x++) 
+    {
+        $result += $set | Get-Random
+    }
+    $begin = "<#`n"
+    $end = "`n#>`n"
+    $final = $begin+$result+$end
+    $randomfilename = -join ((65..90) + (97..122) |
+        Get-Random -Count $suffix |
+        ForEach-Object -Process {
+            [char]$_
+    })
 
-#make a backup of the original files / folders
-$backup = $path + 'backup'
-Copy-Item -Path $path -Destination $backup -Recurse -Force
-$files = Get-ChildItem -Recurse -Path $path -Exclude -neq -Filter *.ps*1
-foreach ($file in $files)
-{
-@($final) + (Get-Content $file) | Set-Content $file
-Rename-Item $file -NewName ($file.BaseName + "-" + $randomfilename + $file.Extension) -Force
-}
-Rename-Item $path $newpath -Force
-Rename-Item $backup $path -Force
+    #make a backup of the original files / folders
+    $backup = $path + 'backup'
+    Copy-Item -Path $path -Destination $backup -Recurse -Force
+    $files = Get-ChildItem -Recurse -Path $path -Exclude -neq -Filter *.ps*1
+    foreach ($file in $files)
+    {
+        @($final) + (Get-Content $file) | Set-Content $file
+        Rename-Item $file -NewName ($file.BaseName + '-' + $randomfilename + $file.Extension) -Force
+    }
+    Rename-Item $path $newpath -Force
+    Rename-Item $backup $path -Force
 }
